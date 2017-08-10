01876921c805c72d98c08aa8dde1bf3f186c3d29
Bclair <bclair@gmail.com>
2017-08-10 10:19:35 -0400

    Practice

---
+++ bark.js
@@ -0,0 +1,12 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + ' says WOOF WOOF');
+    } else {
+        console.log(name + ' says woof woof');
+    }
+}
+
+bark ("Rover", 23);
+bark ("Penny", 10);
+bark ('Justice', 53);
+bark ("Keith", 17);
